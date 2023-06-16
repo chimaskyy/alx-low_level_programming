219f8e530ced5aea50974dbd5a7a6ef9dc5420ef
chimaskyy <chiamakahj2@gmail.com>
2023-06-16 02:20:50 -0700

    Write a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard erro

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korper, 2015-10-19\n", sizeof("and that piece of art is useful\" - Dora Korper, 2015-10-19"));
+return (1);
+}
+
