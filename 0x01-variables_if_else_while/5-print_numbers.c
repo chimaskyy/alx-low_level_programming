c32fd76f7c7644e9a08b3be66ca1d22414cfa9b0
chimaskyy <chiamakahj2@gmail.com>
2023-06-17 02:53:06 -0700

    Correct betty error

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,13 +2,14 @@
 
 /**
  * main - Entry point of program
- * Description 'print single digit of base 10 starting from 0'
- * REturn: Always 0 (success)
+ * Description: 'print single digit of base 10 starting from 0'
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int x;
+
 	x = 0;
 
 	while (x <= 9)
