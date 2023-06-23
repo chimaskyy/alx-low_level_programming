bf5fc76a04f2a2a3a4209bcde0171b67fafa9893
chimaskyy <chiamakahj2@gmail.com>
2023-06-23 07:54:48 -0700

    Write a function that prints the numbers, from 0 to 9 except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,13 +12,9 @@ void print_most_numbers(void)
 	for (c = 0; c <= 9; c++)
 	{
 		if (c == 2 || c == 4)
-		{
 			continue;
-		}
 		else
-		{
-			_putchar(c);
-		}
+			_putchar(48 + c);
 	}
 	_putchar('\n');
 }
