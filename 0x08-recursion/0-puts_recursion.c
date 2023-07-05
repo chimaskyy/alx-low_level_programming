be6704214eebe12db8ab433d1c65bc52341ecdb5
chimaskyy <chiamakahj2@gmail.com>
2023-07-05 12:27:42 -0700

    Update code

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,17 +8,15 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-
-		return;
-
-	else
+	if (*s)
 	{
 
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
+
 
 }
 
