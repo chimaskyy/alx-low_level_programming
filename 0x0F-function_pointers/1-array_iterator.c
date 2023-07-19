c31b68da8c3c41df1208a66a8b06982c4351b2d0
chimaskyy <chiamakahj2@gmail.com>
2023-07-19 07:54:14 -0700

    Update code

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,11 +7,15 @@
  * as a parametr on each element of array
  * @size: Size of the array
  * @action: Function pointer
+ * @array: Element of array pointed to
  */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
+
+	if (array == NULL || action == NULL)
+		return;
 
 	while (i < size)
 	{
