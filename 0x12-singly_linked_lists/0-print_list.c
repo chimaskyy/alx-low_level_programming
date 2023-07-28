095a0b3e4d88dadeb5be54c040ee57abd6f60aa5
chimaskyy <chiamakahj@gmail.com>
2023-07-28 01:51:36 +0100

    Update betty style

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,10 +10,10 @@ size_t print_list(const list_t *h)
 {
 	size_t len = 0;
 	const list_t *temp = h;
-	
+
 	while (temp != NULL)
 	{
-		if(temp->str == NULL)
+		if (temp->str == NULL)
 			printf("[%d] (nil)\n", temp->len);
 		else
 			printf("[%d] %s\n", temp->len, temp->str);
