d21db0d249711b2bb5e5a1645057ffa3bd6ad2fd
chimaskyy <chiamakahj@gmail.com>
2023-07-31 16:00:03 +0100

    Create function to free memory allocated to list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Function that free memory allocated to a list
+ * and setting the head to null
+ * @head: Head of the list pointer to the first element
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *current;
+
+	temp = *head;
+
+	while (temp != NULL)
+	{
+		current = temp->next;
+		free(temp);
+		temp = current;
+	}
+	*head = NULL;
+}
