c3af5c48d7d65be7b4f2b0b67bc3c9075d2f0798
chimaskyy <chiamakahj@gmail.com>
2023-08-03 21:04:56 +0100

    Function that comverts binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * binary_to_uint - Function that converts binary to an unsigned int
+ * @b: Pointer to the string to convert
+ * Return: Converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sum;
+       unsigned int power;
+	int index;
+
+	if (b == NULL)
+		return (0);
+	/* Iterate through the string*/
+	for (index = 0; b[index]; index++)
+	{
+		/*check if any number at each index is not 1 0r 2*/
+		if (b[index] != '0' && b[index] != '1')
+			return (0);
+	}
+	sum = 0;
+	for (power = 1, index--; index >= 0; index--, power *= 2)
+	{
+		if (b[index] == '1')
+			sum += power;
+	}
+
+	return (sum);
+}
+
