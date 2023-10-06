cb08f782180d067ac46bbd09db32903ef5614346
chimaskyy <chiamakahj2@.com>
2023-10-06 14:08:52 +0100

    check

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,31 +8,22 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table;
-	unsigned long int i;
+	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+	unsigned long int i = 0;
 
-	/*Allocate memory for the table and check if mallocing failed*/
-	table = malloc(sizeof(hash_table_t));
-	if (table == NULL)
+	if (hash_table == NULL)
 	{
-
 		return (NULL);
 	}
-	/*Allocate memory to hold the hash nodes(elements)*/
-	table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
-
-	if (table->array == NULL)
+	hash_table->size = size;
+	hash_table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
 	{
-		free(table);
+		free(hash_table);
 		return (NULL);
 	}
-
 
 	for (i = 0; i < size; i++)
-	{
-		/*initialize all element of table to Null*/
-		table->array[i] = NULL;
-	}
-	return (table);
-
+		hash_table->array[i] = NULL;
+	return (hash_table);
 }
