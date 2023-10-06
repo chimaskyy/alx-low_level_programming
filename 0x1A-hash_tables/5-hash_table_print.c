85f855ba8fde579ede15e9bdc2394b6819cafeed
chimaskyy <chiamakahj@gmail.com>
2023-10-06 16:14:04 +0100

    print table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,37 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print hash table
+ * @ht: the hash table to print
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *curr_item;
+	int first_print = 0;
+
+	if (ht == NULL)
+		return;
+
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			curr_item = ht->array[i];
+
+			while (curr_item != NULL)
+			{
+				if (first_print == 1)
+					printf(", ");
+
+				printf("'%s' : '%s'", curr_item->key, curr_item->value);
+				first_print = 1;
+				curr_item = curr_item->next;
+			}
+		}
+	}
+		printf("}\n");
+i}
