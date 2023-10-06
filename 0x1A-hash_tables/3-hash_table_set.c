168f729102c299bd25962cc38d10a2f8e07bf7d5
chimaskyy <chiamakahj@gmail.com>
2023-10-06 14:12:33 +0100

    betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,8 +17,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	idx = key_index((const unsigned char *) key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
-    
-    if (new_node == NULL)
+
+	if (new_node == NULL)
 		return (0);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
@@ -29,16 +29,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
-        if (strcmp(ht->array[idx]->key, key) == 0)
-			{
-				ht->array[idx]->value = strdup(value);
-            }
-            else
-            {
-                new_node->next = ht->array[idx];
-                ht->array[idx] = new_node;
-            }
-		
+		if (strcmp(ht->array[idx]->key, key) == 0)
+		{
+			ht->array[idx]->value = strdup(value);
+		}
+		else
+		{
+			new_node->next = ht->array[idx];
+			ht->array[idx] = new_node;
+		}
+
 	}
 	return (1);
 }
