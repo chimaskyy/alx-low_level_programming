f8c97d65868fdd1e76012663ba3c76baf9f76f31
chimaskyy <chiamakahj@gmail.com>
2024-02-07 11:49:44 +0100

    used recursion

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -23,6 +23,50 @@ void print_array(int *array, size_t first, size_t last)
 }
 
 /**
+ * search - this fuction searches
+ * for a value in a sorted array of integers
+ * using the Binary search algorithm
+ * @array: pointer to the first element
+ * of the array to search
+ * @size: number of elements in array
+ * @value: value to search for
+ * @lower_b: first index of array
+ * @higher_b: last index of array
+ * Return: -1 if element is not found or index of element found
+ */
+
+int search(int *array, size_t size, int value, size_t lower_b, size_t higher_b)
+{
+	size_t mid;
+
+	if (lower_b > higher_b)
+	{
+		return (-1);
+	}
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	mid = (lower_b + higher_b) / 2;
+	printf("Serching in array: ");
+	print_array(array, lower_b, higher_b);
+
+	if (value > array[mid])
+	{
+		return (search(array, size, value, mid + 1, higher_b));
+	}
+	else if (value < array[mid])
+	{
+		return (search(array, size, value, lower_b, mid - 1));
+	}
+	else
+	{
+		return (mid);
+	}
+}
+
+/**
  * binary_search - this fuction searches
  * for a value in a sorted array of integers
  * using the Binary search algorithm
@@ -30,38 +74,10 @@ void print_array(int *array, size_t first, size_t last)
  * of the array to search
  * @size: number of elements in array
  * @value: value to search for
- * Return: -1 if element is not found or index of element found
+ * Return: the value found
  */
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t lower_b = 0;
-	size_t higher_b = size - 1;
-	size_t mid;
-
-	if (array == NULL)
-	{
-		return (-1);
-	}
-
-	while (lower_b <= higher_b)
-	{
-		mid = (lower_b + higher_b) / 2;
-		printf("Serching in array: ");
-		print_array(array, lower_b, higher_b);
-
-		if (value > array[mid])
-		{
-			lower_b = mid + 1;
-		}
-		else if (value < array[mid])
-		{
-			higher_b = mid - 1;
-		}
-		else
-		{
-			return (mid);
-		}
-	}
-	return (-1);
+	return (search(array, size, value, 0, size - 1));
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,6 +6,8 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int search(int *array, size_t size, int value, size_t lower_b, size_t higher_b);
+void print_array(int *array, size_t first, size_t last);	
 /*int findmatch(int *array, int size);*/
 
 #endif
